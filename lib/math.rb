b1889b82c3faa80ce1f797d1c10d5e8f7008632a
Jeremy Gray <jeremygray@utexas.edu>
2019-11-07 09:26:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum=num1+num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  diff=num1-num2
+  diff
 end
 
 def division(num1, num2)
-
+  quotient=num1/num2
+    quotient
 end
 
 def multiplication(num1, num2)
-
+  product=num1*num2
+  product
 end
 
 def modulo(num1, num2)
